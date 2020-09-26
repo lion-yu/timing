e96fcbcf6462ff641ac02d8d1a5967f0669f6088
lion <promeryu@foxmail.com>
2020-09-26 22:56:05 +0800

    引入jquery

--- src/main/java/com/timing/demo/controller/Hello.java
+++ src/main/java/com/timing/demo/controller/Hello.java
@@ -3,10 +3,19 @@ package com.timing.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 86227
+ */
 @RestController
 public class Hello {
     @GetMapping
-    public String hello() {
-        return "hello";
+    public Map<String, String> hello() {
+        Map<String, String> result = new HashMap<>();
+        result.put("name", "张总");
+        result.put("age", "25");
+        return result;
     }
 }
